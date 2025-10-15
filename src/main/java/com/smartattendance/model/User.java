c0741ed515d82d54dd3ebd81744e9c9c945ade8a
ProjektKevin <minthetkhine05@gmail.com>
2025-10-15 16:42:58 +0800

    chore(): formatted the classes

--- src/main/java/com/smartattendance/model/User.java
+++ src/main/java/com/smartattendance/model/User.java
@@ -1,4 +1,23 @@
 package com.smartattendance.model;
-public class User { private final String username, passwordHash, role;
-  public User(String u,String p,String r){ this.username=u; this.passwordHash=p; this.role=r; }
-  public String getUsername(){ return username; } public String getPasswordHash(){ return passwordHash; } public String getRole(){ return role; } }
+
+public class User {
+  private final String username, passwordHash, role;
+
+  public User(String u, String p, String r) {
+    this.username = u;
+    this.passwordHash = p;
+    this.role = r;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public String getRole() {
+    return role;
+  }
+}
