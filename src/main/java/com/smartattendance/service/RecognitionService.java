0965b3b337d98688858dcc5dc55192cdf4227111
Wan Yan <stargaze.sun@gmail.com>
2025-10-18 19:31:19 +0800

    Added postgres to dependency in pom.xml

--- src/main/java/com/smartattendance/service/RecognitionService.java
+++ src/main/java/com/smartattendance/service/RecognitionService.java
@@ -1,30 +1,30 @@
-// // package com.smartattendance.service;
+// package com.smartattendance.service;
 
-// // import java.time.LocalDate;
-// // import java.time.LocalDateTime;
-// // import java.time.LocalTime;
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.time.LocalTime;
 
-// // import com.smartattendance.model.AttendanceRecord;
-// // import com.smartattendance.model.Session;
-// // import com.smartattendance.model.Student;
-// // import com.smartattendance.util.AppContext;
+// import com.smartattendance.model.AttendanceRecord;
+// import com.smartattendance.model.Session;
+// import com.smartattendance.model.Student;
+// import com.smartattendance.util.AppContext;
 
-// // public class RecognitionService {
-// //   // Use shared attendance service so Reports can see the records
-// //   private final AttendanceService attendanceService = AppContext.getAttendanceService();
-// //   public AttendanceService getAttendanceService(){ return attendanceService; }
+// public class RecognitionService {
+//   // Use shared attendance service so Reports can see the records
+//   private final AttendanceService attendanceService = AppContext.getAttendanceService();
+//   public AttendanceService getAttendanceService(){ return attendanceService; }
 
-// //   public void startRecognition(){
-// //     new Thread(() -> {
-// //       try { Thread.sleep(1200); } catch (Exception ignored) {}
-// //       Student s = new Student("S101","Alice Tan","G1");
-// //       Session sess = new Session("SESS1","CS102", LocalDate.now(), LocalTime.of(10,0), LocalTime.of(11,0), "Room 1", 15);
-// //       AttendanceRecord rec = new AttendanceRecord(s, sess, "Present", "Auto", 0.95, LocalDateTime.now());
-// //       attendanceService.markAttendance(rec);
-// //     }).start();
-// //   }
-// //   public void stopRecognition(){}
-// // }
+//   public void startRecognition(){
+//     new Thread(() -> {
+//       try { Thread.sleep(1200); } catch (Exception ignored) {}
+//       Student s = new Student("S101","Alice Tan","G1");
+//       Session sess = new Session("SESS1","CS102", LocalDate.now(), LocalTime.of(10,0), LocalTime.of(11,0), "Room 1", 15);
+//       AttendanceRecord rec = new AttendanceRecord(s, sess, "Present", "Auto", 0.95, LocalDateTime.now());
+//       attendanceService.markAttendance(rec);
+//     }).start();
+//   }
+//   public void stopRecognition(){}
+// }
 
 
 // /*
