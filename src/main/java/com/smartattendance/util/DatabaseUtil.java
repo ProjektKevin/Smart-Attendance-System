94b6e5b0f1bc2cd51d7cc8659f53103db666e33d
williamwinata-smu <w.winata.2024@smu.edu.sg>
2025-10-19 18:42:16 +0800

    Create DatabaseUtil.java

---
+++ src/main/java/com/smartattendance/util/DatabaseUtil.java
@@ -0,0 +1,16 @@
+package com.smartattendance.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?user=postgres.muholwznrkonrcjesdij";
+    private static final String USER = "postgres.muholwznrkonrcjesdij";
+    private static final String PASSWORD = "TheDawn5";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
