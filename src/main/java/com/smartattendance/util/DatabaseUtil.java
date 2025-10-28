506ca26c419201d84c1d55b86919cec311af365c
Thiha Swan Htet <thihash18.23@ichat.sp.edu.sg>
2025-10-28 22:16:03 +0800

    issue(): Fixed pom.xml to run maven
    - Solved cross platform problem
    
    - use maven to clean, compile and run instead of bat files
    
    - Refactored into dotenv package to call sensitive credentials instead of System.getenv()

--- src/main/java/com/smartattendance/util/DatabaseUtil.java
+++ src/main/java/com/smartattendance/util/DatabaseUtil.java
@@ -1,17 +1,23 @@
 package com.smartattendance.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
 
-    private static final String URL = System.getenv("DATABASE_URL");
-    private static final String USER = System.getenv("DATABASE_USER");
-    private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DATABASE_URL");
+    private static final String USER = dotenv.get("DATABASE_USER");
+    private static final String PASSWORD = dotenv.get("DATABASE_PASSWORD");
 
     public static Connection getConnection() {
         try {
+            if (URL == null || USER == null || PASSWORD == null) {
+                throw new SQLException("Missing Database Credentials.");
+            }
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             System.err.println("Failed to connect to database: " + e.getMessage());
