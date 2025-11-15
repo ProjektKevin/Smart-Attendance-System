2d2431ee4f785961fd8a4be927f0346f99fd7b7b
Wan Yan <stargaze.sun@gmail.com>
2025-11-15 21:52:00 +0800

    Added javadoc comments for model/enums/AttendanceStatus, MarkMethod, ToastType

--- src/main/java/com/smartattendance/model/enums/MarkMethod.java
+++ src/main/java/com/smartattendance/model/enums/MarkMethod.java
@@ -1,17 +1,19 @@
-/*
- # Done by: Chue Wan Yan
- # Step: 5
- # Date: 13 Oct 2025
-*/
-
 package com.smartattendance.model.enums;
 
 /**
- * Enum to define how the attendance was marked.
+ * Represents the method used to mark a student's attendance.
+ *
+ * This enum indicates whether attendance was marked automatically by the
+ * system, manually by a user, through QR scanning, or not marked at all
+ * (default).
+ *
+ * @author Chue Wan Yan
+ *
+ * @version 15:11 07 Nov 2025
  */
 public enum MarkMethod {
-    AUTO,    // Marked automatically via face recognition
-    MANUAL,   // Marked manually via user action
-    QR,
-    NONE
+    AUTO, // Marked automatically via face recognition
+    MANUAL, // Marked manually via user action
+    QR, // Marked through a QR code scan
+    NONE // No marking method was applied (default)
 }
--- src/main/java/com/smartattendance/model/enums/ToastType.java
+++ src/main/java/com/smartattendance/model/enums/ToastType.java
@@ -1,6 +1,17 @@
 package com.smartattendance.model.enums;
 
-// F_MA: added by felicia handling marking attendance
+/**
+ * Represents the type of toast notification shown in the application.
+ *
+ * Each type indicates the visual style and purpose of the message
+ * displayed to the user.
+ *
+ * @author Chue Wan Yan
+ *
+ * @version 15:11 07 Nov 2025
+ */
 public enum ToastType {
-    SUCCESS, ERROR, WARNING
+    SUCCESS, // Indicates a successful operation
+    ERROR, // Indicates an error occurs in operation
+    WARNING // Indicates a warning message
 }
